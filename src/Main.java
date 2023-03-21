422767e4b3a1db48743398b614e4c980faf03754
Rastre <rastor13666gmail.com>
2023-03-22 00:42:42 +0300

    hello

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("slave");
-        System.out.println("I am a cat");
-        for (int i = 0; i < 2; i++) {
-            System.out.println("1");
-        }
 
-        System.out.println("first");
-        System.out.println("second");
     }
 }
